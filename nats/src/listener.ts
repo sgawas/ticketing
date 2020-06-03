28a38d794150e32b14fd78d4cd69f6e9b6812ca9
Suraj Gawas <suraj.gawas@nike.com>
2020-06-03 16:26:51 -0700

    nats 4

--- nats/src/listener.ts
+++ nats/src/listener.ts
@@ -1,18 +1,23 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222'
 })
+
+const options = stan.subscriptionOptions()
+                    .setManualAckMode(true);
 
 stan.on('connect', ()=>{
     console.log('listening to Nats');
 
-    const subscription = stan.subscribe('ticket:created');
+    const subscription = stan.subscribe('ticket:created', 'listenerQueueGroup', options);
 
     subscription.on('message', (msg: Message)=>{
         const data = msg.getData();
 
         console.log(`Received message#: ${msg.getSequence()} with data: ${data}`);
+        msg.ack();
     })
 })
