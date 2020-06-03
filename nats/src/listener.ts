ae2836a7f590e9f23372328e131c67997671f57b
Suraj Gawas <suraj.gawas@nike.com>
2020-06-03 15:10:11 -0700

    nats 2

---
+++ nats/src/listener.ts
@@ -0,0 +1,16 @@
+import nats from 'node-nats-streaming';
+
+console.clear();
+const stan = nats.connect('ticketing', '123', {
+    url: 'http://localhost:4222'
+})
+
+stan.on('connect', ()=>{
+    console.log('listening to Nats');
+
+    const subscription = stan.subscribe('ticket:created');
+
+    subscription.on('message', (msg)=>{
+        console.log('message received');
+    })
+})
--- nats/src/publisher.ts
+++ nats/src/publisher.ts
@@ -1,5 +1,6 @@
 import nats from 'node-nats-streaming';
 
+console.clear();
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 })
