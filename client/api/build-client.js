b9b079082e47bf16b9e7b19848d02b615410f640
Suraj Gawas <suraj.gawas@nike.com>
2020-08-11 15:33:51 -0700

    baseurl update

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,8 @@ export default ({ req }) => {
     // We are on the server. windows object is only defined on browser not on node.
     // reachout to service on other namespace. nextjs needs to reach ingress-nginx namespace and service ingress-nginx so to finally call auth service.
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+    //  baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.ticketing-app.xyz/',
       headers: req.headers
     });
   } else {
