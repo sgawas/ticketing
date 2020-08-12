5621e537c61104b9669b399d27b45f6e9ea913b5
Suraj Gawas <suraj.gawas@nike.com>
2020-08-12 16:54:59 -0700

    commit 8/11 -3

--- nats/src/events/ticket-created-publisher.ts
+++ nats/src/events/ticket-created-publisher.ts
@@ -1,4 +1,6 @@
-import { Publisher, TicketCreatedEvent, Subjects } from "@surajng/common";
+import { Publisher } from "./base-publisher";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
     readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;   
